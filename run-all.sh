94c8d5146b4252cb0525ee78569f194675b71dbd
Luuk Schoenmakers <luuk.schoenmakers@gmail.com>
2014-12-11 00:31:28 +0100

    added a gitignore and a run-all script. Schedule this script through crontab at midnight

--- run-all.sh
+++ run-all.sh
@@ -1,15 +1,9 @@
+#!/bin/sh
 #this script is for running all the scraper stuff at once
 
-#enter Alternate
-cd Alternate
-scrapy crawl alternatecases
-scrapy crawl alternateharddisk
-scrapy crawl alternatemoederbord
-scrapy crawl alternatevoeding
-
-#go one directory back, pick up the next one
-cd ..
-cd Informatique
+cd /Users/luukschoenmakers/Documents/studie/INFPRJ56/pc_builder_crawler/Informatique
+PATH=$PATH:/usr/local/bin
+export PATH
 scrapy crawl cases
 scrapy crawl fans
 scrapy crawl harddisks
@@ -21,3 +15,15 @@ scrapy crawl ramddr3
 scrapy crawl ramddr4
 scrapy crawl soundcards
 
+cd /Users/luukschoenmakers/Documents/studie/INFPRJ56/pc_builder_crawler/Alternate
+
+scrapy crawl alternatecases
+scrapy crawl alternateharddisk
+scrapy crawl alternatemoederbord
+scrapy crawl alternatevoeding
+scrapy crawl alternatekoeling
+scrapy crawl alternateram1
+scrapy crawl alternateram2
+scrapy crawl alternateram3
+scrapy crawl alternateram4
+scrapy crawl alternategeluidskaart
